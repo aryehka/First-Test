5486a94143ef3063141ccc6271a6583e5e9f94a1
@aryehk <aryehka@gmail.com>
2017-08-09 19:45:51 +0300

    added hi

--- addemail.php
+++ addemail.php
@@ -1,13 +1,13 @@
 <?php
-$firstname=$_POST['firstname'];
-$lastname=$_POST['lastname'];
+$first_name=$_POST['firstname'];
+$last_name=$_POST['lastname'];
 $email=$_POST['email'];
 
 $start=mysqli_connect('localhost','elmer','ayfoinUp9ZRGeYHV','elmer_emails') or die ("Error connecting to MySQL database");
-$query="INSER INTO subscribers (first_name, last_name, email)"." VALUES ('$firstname', '$lastname','$email')";
+$query="INSERT INTO subscribers (first_name, last_name, email) VALUES ('$first_name', '$last_name','$email')";
 
 $result=mysqli_query($start,$query) or die ("Error querying DB");
 
 mysqli_close($start);
 
-echo "Form recieved. Thank you ".$firstname.".";
+echo "Form recieved. Thank you ".$first_name.".";
