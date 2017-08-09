7100708f50c49545b93fd943970b43c8f907e06c
@aryehk <aryehka@gmail.com>
2017-08-09 19:33:22 +0300

    added hi

---
+++ addemail.php
@@ -0,0 +1,13 @@
+<?php
+$firstname=$_POST['firstname'];
+$lastname=$_POST['lastname'];
+$email=$_POST['email'];
+
+$start=mysqli_connect('localhost','elmer','ayfoinUp9ZRGeYHV','elmer_emails') or die ("Error connecting to MySQL database");
+$query="INSER INTO subscribers values ('$firstname', '$lastname','$email')";
+
+$result=mysqli_query($start,$query) or die ("Error querying DB");
+
+mysqli_close($start);
+
+echo "Form recieved. Thank you ".$firstname.".";
